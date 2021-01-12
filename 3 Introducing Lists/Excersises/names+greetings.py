d493ca6d81d2ef26c8766010ec181a9a3dfc3c79
jwood91 <47952114+jwood91@users.noreply.github.com>
2021-01-12 12:57:58 +0000

    Add files via upload

---
+++ 3 Introducing Lists/Excersises/names+greetings.py
@@ -0,0 +1,10 @@
+# names excersize
+names = ['jack', 'dylan', 'joe', 'james']
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
+print(names[3].title())
+
+# Greetings Excersize
+print("Hi there " + names[0].title() + " my man")
+print("Whats up " + names[1].title() + " hows it going?")
